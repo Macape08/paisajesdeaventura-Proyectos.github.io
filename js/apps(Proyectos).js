33182dba436da9fdccdfae33630bdd11c09aeb65
Manuel Caballol <mbullas08@gmail.com>
2021-01-03 16:34:34 +0100

    Paisajesdeaventura | Proyectos Activos

---
+++ js/apps(Proyectos).js
@@ -0,0 +1,85 @@
+let btnMenu = document.querySelector(".btn-menu");
+let menu = document.querySelector(".list-container");
+let containerMenu = document.querySelector(".menu");
+let activador = true;
+
+// Menu de navegacion
+
+btnMenu.addEventListener("click", () => {
+
+    document.querySelector(".btn-menu i").classList.toggle("fa-times");
+
+    if(activador){
+       
+    menu.style.left = "0";
+    menu.style.transition = "0.5";
+
+    activador = false;
+
+    }else{
+        activador = false;
+    menu.style.left = "-100%";
+    menu.style.transition = "0.5";
+    
+        activador = "true"
+
+    }
+
+});
+
+
+
+// Intercalar clase "activo"
+
+let enlaces = document.querySelectorAll(".lists li a")
+
+enlaces.forEach( (element) => {
+
+    element.addEventListener("click", (event) => {
+
+        enlaces.forEach( (link) => {
+            link.classList.remove("activo")
+        });
+
+        event.target.classList.add("activo")
+
+    });
+
+});
+
+// Efectos Scroll
+
+let prevScrollPos = window.pageYOffset;
+let goTop = document.querySelector("go-top");
+
+window.onscroll = () => {
+    
+    let currenScrollPos = window.pageYOffset
+
+    // Mostrar y ocultar menu
+    if (prevScrollPos > currenScrollPos) {
+        containerMenu.style.top = "0";
+        containerMenu.style.transition = "0.5s";
+    }else{
+        containerMenu.style.top = "-60px";
+        containerMenu.style.transition = "0.5s";
+    }
+
+    prevScrollPos = currenScrollPos
+
+    // Mostrar y ocultar scroll Estilos
+
+    let arriba = window.pageYOffset;
+
+    if (arriba <= 600) {
+        containerMenu.style.borderBottom = "none";
+        
+        goTop.style.left = "-100%";
+
+    }else{
+        containerMenu.style.borderBottom = "2px solid #ff2e63";
+    
+     goTop.style.left = "0";
+     goTop.style.transition = "0.5s";  
+    }
+}
